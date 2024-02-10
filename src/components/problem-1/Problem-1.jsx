6f125610171fd6b980cce0d7bade01aa76e42202
NRS\nrsha <nrshakib007@gmail.com>
2024-02-11 00:53:19 +0600

    added Task Page

---
+++ src/components/problem-1/Problem-1.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Tasks from "./Tasks";
+
+const Problem1 = () => {
+  return <Tasks />;
+};
+
+export default Problem1;
---
+++ src/components/problem-1/Tasks.jsx
@@ -0,0 +1,27 @@
+const Tasks = () => {
+  return (
+    <div>
+      <div className="col-8">
+        <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+          <li>
+            <button>All</button>
+          </li>
+          <li>
+            <button>Active</button>
+          </li>
+          <li>
+            <button>Completed</button>
+          </li>
+          <li>
+            <button>Pending</button>
+          </li>
+          <li>
+            <button>Archive</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Tasks;
