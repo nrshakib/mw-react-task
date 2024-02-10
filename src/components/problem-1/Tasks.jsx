23aa3c3278b08ac27a0ec90d468bf56db1bc10d4
NRS\nrsha <nrshakib007@gmail.com>
2024-02-11 01:36:00 +0600

    Added Task Filtering Functions and Styles

--- src/components/problem-1/Tasks.jsx
+++ src/components/problem-1/Tasks.jsx
@@ -4,6 +4,7 @@ import TaskList from "./TaskList";
 
 const Tasks = () => {
   const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("all");
 
   //Save tasks to localStorage whenever tasks state changes
   useEffect(() => {
@@ -22,25 +23,65 @@ const Tasks = () => {
     setTasks([...tasks, newtask]);
   };
 
+  // Function to filter Tasks
+  const filtertasks = () => {
+    switch (filter) {
+      case "complete":
+        return tasks.filter((task) => task.status === "complete");
+      case "active":
+        return tasks.filter((task) => task.status === "active");
+      case "pending":
+        return tasks.filter((task) => task.status === "pending");
+      case "archive":
+        return tasks.filter((task) => task.status === "archive");
+      default:
+        return tasks;
+    }
+  };
   return (
     <div>
       <TaskForm addTask={addTask} />
       <div className="col-8">
         <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
           <li>
-            <button>All</button>
+            <button
+              onClick={() => setFilter("all")}
+              className={`nav-link ${(filter === "all") & "active"}`}
+            >
+              All
+            </button>
           </li>
           <li>
-            <button>Active</button>
+            <button
+              onClick={() => setFilter("active")}
+              className={`nav-link ${(filter === "active") & "active"}`}
+            >
+              Active
+            </button>
           </li>
           <li>
-            <button>Completed</button>
+            <button
+              onClick={() => setFilter("complete")}
+              className={`nav-link ${(filter === "complete") & "active"}`}
+            >
+              Completed
+            </button>
           </li>
           <li>
-            <button>Pending</button>
+            <button
+              onClick={() => setFilter("pending")}
+              className={`nav-link ${(filter === "pending") & "active"}`}
+            >
+              Pending
+            </button>
           </li>
           <li>
-            <button>Archive</button>
+            <button
+              onClick={() => setFilter("archive")}
+              className={`nav-link ${(filter === "archive") & "active"}`}
+            >
+              Archive
+            </button>
           </li>
         </ul>
       </div>
