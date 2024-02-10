1429104983a38349b59472e04dfc32ed4926fdfb
NRS\nrsha <nrshakib007@gmail.com>
2024-02-11 02:12:43 +0600

    Added Routes for Modals

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Menu from "./components/Menu.jsx";
 import Index from "./components/Index.jsx";
 import Problem1 from "./components/problem-1/Problem-1.jsx";
 import Problem2 from "./components/problem-2/Problem-2.jsx";
+import ModalA from "./components/problem-2/components/ModalA.jsx";
+import ModalB from "./components/problem-2/components/ModalB.jsx";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Menu />}>
           <Route path="problem-1" element={<Problem1 />} />
           <Route path="problem-2" element={<Problem2 />} />
+          <Route path="problem-2/all-contacts" element={<ModalA />} />
+          <Route path="problem-2/us-contacts" element={<ModalB />} />
         </Route>
       </Routes>
     </>
