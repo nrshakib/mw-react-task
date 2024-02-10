0708a75d0c6219c3923e1af3b7f59574adbe9c4f
NRS\nrsha <nrshakib007@gmail.com>
2024-02-11 01:50:16 +0600

    Problem 2 initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu.jsx";
-import Problem2 from "./components/Problem-2.jsx";
 import Index from "./components/Index.jsx";
 import Problem1 from "./components/problem-1/Problem-1.jsx";
+import Problem2 from "./components/problem-2/Problem-2.jsx";
 
 function App() {
   return (
