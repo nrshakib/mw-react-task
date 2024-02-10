76d31f3ef406f0ec60be596ad5becb9e3ab5de12
NRS\nrsha <nrshakib007@gmail.com>
2024-02-11 01:20:25 +0600

    Functions added to Add Task,Store to and Fetch from the Local Storage

--- src/components/problem-1/Tasks.jsx
+++ src/components/problem-1/Tasks.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import TaskForm from "./TaskForm";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+
+  //Save tasks to localStorage whenever tasks state changes
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  //Fetch data from localstorage
+  useEffect(() => {
+    const storedtasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    console.log(storedtasks);
+    setTasks(storedtasks);
+  }, []);
+
+  // Set New tasks to Previous Task List
+  const addTask = (newtask) => {
+    setTasks([...tasks, newtask]);
+  };
+
   return (
     <div>
-      <TaskForm />
+      <TaskForm addTask={addTask} />
       <div className="col-8">
         <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
           <li>
